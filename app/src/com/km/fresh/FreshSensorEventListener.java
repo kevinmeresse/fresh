1c2e1f1f5607bf7cfd6db97aa872f041cc512880
Kevin Meresse <kevin.meresse@gmail.com>
2014-04-19 18:21:56 -0700

    Raw data saved and new count added

--- app/src/com/km/fresh/FreshSensorEventListener.java
+++ app/src/com/km/fresh/FreshSensorEventListener.java
@@ -3,6 +3,8 @@ package com.km.fresh;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
 
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -15,6 +17,8 @@ public class FreshSensorEventListener implements SensorEventListener {
     private static final String TAG = "SensorListener";
     private static final float MOVEMENT_THRESHOLD = 0.1f;
     private static final float FLUSH_INTERVAL = 60000; //1 minute
+    private static final String DATA_RAW = "raw";
+    private static final String DATA_COUNT = "count";
 
     //#### Vars ####
     private float lastX  = 0.0f;
@@ -23,40 +27,39 @@ public class FreshSensorEventListener implements SensorEventListener {
     private float lastXDiff  = 0.0f;
     private float lastYDiff  = 0.0f;
     private float lastZDiff  = 0.0f;
-    private int mCountMaj = 0;
-    private int mCountMaj2 = 0;
-    private int mCountMaj3 = 0;
-    private int mCountKev = 0;
+    private int simpleThresholdCount = 0;
+    private int changeDirectionCount = 0;
+    private int changeDirectionThresholdCount = 0;
+    private int positiveNegativeCount = 0;
+    private int averageOfSevenPreviousCount = 0;
     private long tsOfLastFlush = 0l;
     private static final long uniqueFileId = System.currentTimeMillis();
+    private Queue<Integer> sevenPrevious = new LinkedList<Integer>();
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-//        Log.d(TAG, "onSensorChanged: Values: "
-//                + "x:" + event.values[0]
-//                + ", y:" + event.values[1]
-//                + ", z:" + event.values[2]
-//                + " Type: " + getNameForSensorType(event.sensor.getType()));
 
         long now = System.currentTimeMillis();
 
+        // Increment when moving more than threshold
         if (Math.abs(event.values[0] - lastX) > MOVEMENT_THRESHOLD
                 || Math.abs(event.values[1] - lastY) > MOVEMENT_THRESHOLD
                 || Math.abs(event.values[2] - lastZ) > MOVEMENT_THRESHOLD) {
-            mCountMaj++;
+        	simpleThresholdCount++;
         }
 
+        // Increment when changing movement direction
         if (((event.values[0] - lastX) > 0 && lastXDiff < 0)
                 || ((event.values[1] - lastY) > 0 && lastYDiff < 0)
                 || ((event.values[2] - lastZ) > 0 && lastZDiff < 0)
                 || ((event.values[0] - lastX) < 0 && lastXDiff > 0)
                 || ((event.values[1] - lastY) < 0 && lastYDiff > 0)
-                || ((event.values[2] - lastZ) < 0 && lastZDiff > 0)){
-            mCountMaj2++;
+                || ((event.values[2] - lastZ) < 0 && lastZDiff > 0)) {
+        	changeDirectionCount++;
         }
 
-        if (
-            ((((event.values[0] - lastX) > 0 && lastXDiff < 0)
+        // Increment when changing movement direction AND movement is more than threshold
+        if (((((event.values[0] - lastX) > 0 && lastXDiff < 0)
                     || ((event.values[0] - lastX) < 0 && lastXDiff > 0))
                         && Math.abs(event.values[0] - lastX) > MOVEMENT_THRESHOLD)
             ||
@@ -66,18 +69,22 @@ public class FreshSensorEventListener implements SensorEventListener {
             ||
             ((((event.values[2] - lastZ) > 0 && lastZDiff < 0)
                 || ((event.values[2] - lastZ) < 0 && lastZDiff > 0))
-                    && Math.abs(event.values[2] - lastZ) > MOVEMENT_THRESHOLD)){
-            mCountMaj3++;
+                    && Math.abs(event.values[2] - lastZ) > MOVEMENT_THRESHOLD)) {
+        	changeDirectionThresholdCount++;
         }
 
+        // Increment when switching from positive to negative (and vice-versa) 
         if ((event.values[0] > 0 && lastX < 0)
                 || (event.values[1] > 0 && lastY < 0)
                 || (event.values[2] > 0 && lastZ < 0)
                 || (event.values[0] < 0 && lastX > 0)
                 || (event.values[1] < 0 && lastY > 0)
-                || (event.values[2] < 0 && lastZ > 0)){
-            mCountKev++;
+                || (event.values[2] < 0 && lastZ > 0)) {
+        	positiveNegativeCount++;
         }
+        
+        // Write logs for RAW data
+        logDataToSdCard(DATA_RAW, now + "," + event.values[0] + "," + event.values[1] + "," + event.values[2] + "\n");
 
 //        Log.d(TAG, "onSensorChanged: Values: "
 //                + "x:" + Math.abs(event.values[0] - lastX)
@@ -88,6 +95,7 @@ public class FreshSensorEventListener implements SensorEventListener {
 //                + ",\tnow:" + now
 //                + ",\ttsOfLastFlush:" + tsOfLastFlush);
 
+        // Remember values
         lastXDiff = event.values[0] - lastX;
         lastYDiff = event.values[1] - lastY;
         lastZDiff = event.values[2] - lastZ;
@@ -98,19 +106,27 @@ public class FreshSensorEventListener implements SensorEventListener {
         if (tsOfLastFlush == 0l) {
             tsOfLastFlush = now;
         }
+        
+        // Flush count every FLUSH_INTERVAL time
         if ((now - tsOfLastFlush) > FLUSH_INTERVAL) {
             tsOfLastFlush = now;
-            Log.d(TAG, "FLUSH: " + now + " | " + "countMaj = " + mCountMaj + " \t countMaj2 = " + mCountMaj2 + " \t countMaj3 = " + mCountMaj3 + " \t countKev = " + mCountKev);
-
-            logDataToSdCard(now + "," + mCountMaj + "," + mCountMaj2 + "," + mCountMaj3 + "," + mCountKev + "\n");
-            mCountMaj = 0;
-            mCountMaj2 = 0;
-            mCountMaj3 = 0;
-            mCountKev = 0;
+            
+            // Calculate average for 7 previous minutes
+            averageOfSevenPreviousCount = calculateSevenPreviousAverage();
+            
+            // Write logs for COUNT
+            Log.d(TAG, "FLUSH COUNT: " + now + " | " + "Simple threshold = " + simpleThresholdCount + " \t Change direction = " + changeDirectionCount + " \t Change direction + threshold = " + changeDirectionThresholdCount + " \t Positive to negative = " + positiveNegativeCount + " \t Average of 7 previous = " + averageOfSevenPreviousCount);
+            logDataToSdCard(DATA_COUNT, now + "," + simpleThresholdCount + "," + changeDirectionCount + "," + changeDirectionThresholdCount + "," + positiveNegativeCount + "," + averageOfSevenPreviousCount + "\n");
+            
+            // Reset count values
+            simpleThresholdCount = 0;
+            changeDirectionCount = 0;
+            changeDirectionThresholdCount = 0;
+            positiveNegativeCount = 0;
         }
     }
 
-    public void logDataToSdCard(String line) {
+    public void logDataToSdCard(String dataType, String line) {
         FileOutputStream fos = null;
 
         try {
@@ -120,7 +136,12 @@ public class FreshSensorEventListener implements SensorEventListener {
                 dir.mkdirs();
             }
 
-            final File myFile = new File(dir, "Fresh_data_" + uniqueFileId + ".txt");
+            File myFile = null;
+            if (DATA_RAW.equals(dataType)) {
+            	myFile = new File(dir, "Fresh_data_raw_" + uniqueFileId + ".csv");
+            } else {
+            	myFile = new File(dir, "Fresh_data_count_" + uniqueFileId + ".csv");
+            }
 
             if (!myFile.exists()) {
                 myFile.createNewFile();
@@ -133,21 +154,23 @@ public class FreshSensorEventListener implements SensorEventListener {
             e.printStackTrace();
         }
     }
+    
+    public int calculateSevenPreviousAverage() {
+    	sevenPrevious.add(simpleThresholdCount);
+    	if (sevenPrevious.size() == 0) {
+        	return simpleThresholdCount;
+        } else if (sevenPrevious.size() > 7) {
+        	sevenPrevious.poll();
+        }
+        int sum = 0;
+        for (Integer value : sevenPrevious) {
+			sum += value;
+		}
+        
+        return sum / sevenPrevious.size();
+    }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-    }
-
-
-    private String getNameForSensorType(int type) {
-        switch (type) {
-        case Sensor.TYPE_ACCELEROMETER:
-            return "Accelerometer";
-        case Sensor.TYPE_GYROSCOPE:
-            return "Gyroscope";
-        case Sensor.TYPE_MAGNETIC_FIELD:
-            return "Magnetic Field";
-        }
-        return "Other";
     }
 }
